4b6d2821467e95a0bee3abfa8b49163a55e8c63c
kmahi03 <montya.kamlesh@gmail.com>
2024-06-05 12:33:40 -0400

    Initial commit

---
+++ rest-soap-service-integration/src/main/java/com/esh/map/RestSoapServiceIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.esh.map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSoapServiceIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSoapServiceIntegrationApplication.class, args);
+	}
+
+}
